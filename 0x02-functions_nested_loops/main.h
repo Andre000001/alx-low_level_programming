0875c4580e16f69b35e1eb82390619a07286a8e2
Andre000001 <ndukwecaleb36@gmail.com>
2023-07-18 20:33:18 -0700

    01-natural.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,21 +1,4 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
-/**
- * print_times_table - Prints the n times table, starting with 0.
- * @n: The number of times table to print.
- */
-void print_times_table(int n);
-
-/**
- * _putchar - Writes a character to stdout.
- * @c: The character to be written.
- *
- * Return: On success, the character written is returned.
- * On error, -1 is returned.
- */
-int _putchar(char c);
-
 #endif /* MAIN_H */
