b6a69189ced6db9a0a46cc3abe4539f6b9608854
Andre000001 <ndukwecaleb36@gmail.com>
2023-07-24 06:19:23 -0700

    the coding experince was dope, i tried my best but i don't think i'll be here next month

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 - Takes a pointer to an int as a parameter and updates
+ *               the value it points to 98.
+ *
+ * @n: Pointer to the integer to be updated.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0x05-pointers_arrays_strings is not that hard if i try harder
