16cccdf9df901c23aacd82230fafa75df4e9fc70
NDUKWE CALEB <ndukwecaleb3@gmail.com>
2023-09-12 16:26:18 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,7 @@
 /* Prototype for the _putchar function */
 int _putchar(char c);
 
-/* Prototype for print_alphabet function */
-void print_alphabet(void);
+/* Prototype for print_alphabet_x10 function */
+void print_alphabet_x10(void);
 
 #endif /* MAIN_H */
