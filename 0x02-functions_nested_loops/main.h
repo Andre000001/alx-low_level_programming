2e405d81195eb10b91a7e7574867923ec4aec404
NDUKWE CALEB <ndukwecaleb3@gmail.com>
2023-09-12 16:44:26 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Prototype for the _abs function */
-int _abs(int n);
+/* Prototype for the _putchar function */
+int _putchar(char c);
+
+/* Prototype for the print_last_digit function */
+int print_last_digit(int n);
 
 /* You can add more function prototypes or declarations here if needed */
 
