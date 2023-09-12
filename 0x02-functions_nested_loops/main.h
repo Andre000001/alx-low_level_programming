ee0c045ec9be271ef542bc3c1dfa1e187c67b2d8
NDUKWE CALEB <ndukwecaleb3@gmail.com>
2023-09-12 16:41:48 -0700

    6-abs.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Prototype for the _putchar function */
-int _putchar(char c);
-
-/* Prototype for the _print_sign function */
-int print_sign(int n);
+/* Prototype for the _abs function */
+int _abs(int n);
 
 /* You can add more function prototypes or declarations here if needed */
 
