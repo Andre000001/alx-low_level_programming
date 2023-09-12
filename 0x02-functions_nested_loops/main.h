e61cd1478d2b826256090673b6284d545c3cf790
NDUKWE CALEB <ndukwecaleb3@gmail.com>
2023-09-12 15:51:36 -0700

    alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
-/* Function prototype for the _putchar function */
+/* Prototype for the _putchar function */
 int _putchar(char c);
 
-/* Function prototype for the print_times_table function */
-void print_times_table(int n);
+/* Prototype for print_alphabet function */
+void print_alphabet(void);
 
 #endif /* MAIN_H */
