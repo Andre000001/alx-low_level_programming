9412506d98ab7bb7502f03deb6a9c21cedcd6cac
NDUKWE CALEB <ndukwecaleb3@gmail.com>
2023-09-12 17:39:40 -0700

    this my 11th prject under this tasks

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Function prototypes */
+void print_to_98(int n);
 int add(int a, int b);
+/* Add more function prototypes here as needed */
 
 #endif /* MAIN_H */
