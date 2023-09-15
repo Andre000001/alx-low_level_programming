bc4e038f6f5d82bded98cd1ab25204b04143f6e8
NDUKWE CALEB <ndukwecaleb3@gmail.com>
2023-09-15 05:14:10 -0700

    completed task

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int mul(int a, int b);
+/* Function prototype for _putchar */
+int _putchar(char c);
+
+/* Function prototype for print_numbers */
+void print_numbers(void);
 
 #endif /* MAIN_H */
