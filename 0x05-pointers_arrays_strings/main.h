405e75ff1f72b238d009eca7cd776a5043b7c1e4
NDUKWE CALEB <ndukwecaleb3@gmail.com>
2023-09-18 17:00:36 -0700

    1-swap

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,19 +2,7 @@
 #define MAIN_H
 
 /* Function prototypes */
+void swap_int(int *a, int *b);
 void reset_to_98(int *n);
-
-/* Constants and Macros */
-#define MAX_VALUE 100
-#define DEBUG_MODE 1
-
-/* Custom data type declarations */
-typedef struct {
-    int x;
-    int y;
-} Point;
-
-/* External variable declaration */
-extern int globalVar;
 
 #endif /* MAIN_H */
