9332b9adf92c06bc7a98af55213676920c233995
NDUKWE CALEB <ndukwecaleb3@gmail.com>
2023-09-20 17:55:40 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,9 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h> /* Include necessary standard library headers */
-
-/* Function prototypes */
-void reverse_array(int *a, int n);
+/* Function prototype for string_toupper */
+char *string_toupper(char *str);
 
 #endif /* MAIN_H */
