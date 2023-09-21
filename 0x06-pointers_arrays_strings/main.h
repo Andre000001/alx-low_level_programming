593e0720255c497ee07a2cddf7af4bee251c32d2
NDUKWE CALEB <ndukwecaleb3@gmail.com>
2023-09-20 17:46:41 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _strcmp(char *s1, char *s2);
+#include <stdio.h> /* Include necessary standard library headers */
+
+/* Function prototypes */
+void reverse_array(int *a, int n);
 
 #endif /* MAIN_H */
