fedaeb20cf8ef0e31418e022eb98eb319053082c
NDUKWE CALEB <ndukwecaleb3@gmail.com>
2023-09-20 17:18:29 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,13 +1,6 @@
-#ifndef STRCAT_H
-#define STRCAT_H
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * _strcat - Concatenates two strings.
- * @dest: The destination string.
- * @src: The source string to append to dest.
- *
- * Return: A pointer to the resulting string dest.
- */
-char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
-#endif /* STRCAT_H */
+#endif /* MAIN_H */
