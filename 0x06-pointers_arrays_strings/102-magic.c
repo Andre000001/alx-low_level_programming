7e61d16c2a076c41ca89e73c6c07a77513f01dbd
NDUKWE CALEB <ndukwecaleb3@gmail.com>
2023-09-21 18:56:17 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	int a[5];
+int a[5];
 
-	a[2] = 1024;
-	*(a + 2) = 98;
-	/* your code goes here */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+a[2] = 102;
+*(a + 2) = 98;
+printf("a[2] = %d\n", a[2]);
+return (0);
 }
