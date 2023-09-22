beb86e028719b23075e6ba3906219aa9beb10cb4
NDUKWE CALEB <ndukwecaleb3@gmail.com>
2023-09-21 18:43:30 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,61 +1,46 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * infinite_add - Adds two numbers stored as strings
- * @n1: The first number as a string
- * @n2: The second number as a string
- * @r: The buffer to store the result
- * @size_r: The size of the buffer
+ * infinite_add - Adds two numbers.
+ * @n1: The first number as a string.
+ * @n2: The second number as a string.
+ * @r: The buffer to store the result.
+ * @size_r: The size of the buffer.
  *
- * Return: If the result can be stored in `r`, return `r`, else return `0`
+ * Return: A pointer to the result, or 0 if the result cannot be stored in r.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, carry = 0, len1 = 0, len2 = 0, max_len;
-	char *temp;
+	int len1, len2, carry, sum, i, j;
 
-	while (n1[len1] != '\0')
-		len1++;
-	while (n2[len2] != '\0')
-		len2++;
+	/* Calculate the lengths of n1 and n2 */
+	for (len1 = 0; n1[len1]; len1++)
+		;
+	for (len2 = 0; n2[len2]; len2++)
+		;
 
-	max_len = (len1 > len2) ? len1 : len2;
-
-	if (max_len + 1 > size_r)
+	if (len1 + 2 > size_r || len2 + 2 > size_r)
 		return (0);
 
-	n1 += len1 - 1;
-	n2 += len2 - 1;
+	carry = 0;
+	i = len1 - 1;
+	j = len2 - 1;
+	r[size_r - 1] = '\0';
 
-	temp = r + max_len + 1;
-	*temp = '\0';
-
-	for (i = max_len; i >= 0; i--)
+	while (i >= 0 || j >= 0 || carry)
 	{
-		int sum = carry;
+		int digit1 = (i >= 0) ? n1[i] - '0' : 0;
+		int digit2 = (j >= 0) ? n2[j] - '0' : 0;
 
-		if (len1 > 0)
-		{
-			sum += (*n1 - '0');
-			n1--;
-			len1--;
-		}
-
-		if (len2 > 0)
-		{
-			sum += (*n2 - '0');
-			n2--;
-			len2--;
-		}
-
+		sum = digit1 + digit2 + carry;
 		carry = sum / 10;
-		*temp = (sum % 10) + '0';
-		temp--;
+		r[size_r - 2] = (sum % 10) + '0';
+
+		i--;
+		j--;
+		size_r--;
 	}
 
-	if (*temp == '0')
-		temp++;
-
-	return (temp);
+	return (r + size_r);
 }
