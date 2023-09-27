3344ff2ec019b336081587a39f9243a57306052b
NDUKWE CALEB <ndukwecaleb3@gmail.com>
2023-09-26 17:59:59 -0700

    1-memcpy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+#include <stdio.h>  /* Include standard I/O library for printf function */
 
+/* Function prototypes */
+char *_memcpy(char *dest, char *src, unsigned int n);
 void simple_print_buffer(char *buffer, unsigned int size);
-char *_memset(char *s, char b, unsigned int n);
 
 #endif /* MAIN_H */
