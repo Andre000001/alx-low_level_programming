9b741e292bef6a825e2ff49b4a537b314920e140
NDUKWE CALEB <ndukwecaleb3@gmail.com>
2023-10-01 21:12:23 -0700

    0x08. C - Recursion

---
+++ 0x08-recursion/101-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+    int r;
+
+    r = wildcmp("main.c", "*.c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m*a*i*n*.*c*");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "main.c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m*c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "ma********************************c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "*");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "***");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m.*c");
+    printf("%d\n", r);
+    r = wildcmp("main.c
